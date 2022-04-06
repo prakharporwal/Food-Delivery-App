a0aaf34f34f4b097b1f68462c0fcab1cfccf9a33
prakharporwal <prakharporwal@github>
2022-04-06 21:13:49 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
+import MainPage from './Pages/MainPage';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
@@ -18,7 +18,9 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+
+      <MainPage></MainPage>
     </div>
   );
 }
---
+++ src/Pages/MainPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './MainPage.css';
+
+export default function MainPage(){
+    return (
+        <div className="main-app">
+            <text>
+                Work In Progress
+            </text>
+        </div>
+    )
+}
